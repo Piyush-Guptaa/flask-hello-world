f4898a7afb06acbcc11a4456e34de9f64568bd40
Piyush Gupta <77025176+Piyush-Guptaa@users.noreply.github.com>
2023-03-18 16:18:29 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,20 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import requests
+import os
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+@app.route('/AdminDetails/<path:path>', methods=['GET', 'POST', 'PUT', 'DELETE'])
+def proxy(path):
+    url = f"https://bookingadmin-production.up.railway.app/AdminDetails/{path}"
+    method = request.method.lower()
+    headers = {'Content-Type': 'application/json'}
+    data = request.get_json() if request.method in ['POST', 'PUT'] else None
 
-@app.route('/about')
-def about():
-    return 'About'
+    response = requests.request(method, url, headers=headers, json=data)
+    if response.status_code != 200:
+        return jsonify(error='Failed to retrieve data'), 500
+    return jsonify(response.json())
+
+if __name__ == '__main__':
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
